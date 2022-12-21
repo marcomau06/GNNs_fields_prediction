25e851aec74ec031e3d8a900a17e1e9206642f96
Marco Maurizi <64018412+marcomau06@users.noreply.github.com>
2022-12-21 19:57:13 +0100

    Create U_net.py

---
+++ Image-based U-Net/U_net.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  2 22:49:21 2022
+
+@author: marcomau
+"""
+import torch
+import torch.nn as nn
+import torchvision
+
+class Block(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_ch, out_ch, 9, padding = 'same')
+        self.relu  = nn.ReLU()
+        self.conv2 = nn.Conv2d(out_ch, out_ch, 9, padding = 'same')
+    
+    def forward(self, x):
+        return self.conv2(self.relu(self.conv1(x)))
+
+
+class Encoder(nn.Module):
+    def __init__(self, chs=(2,16,32,64,128,256)):
+        super().__init__()
+        self.enc_blocks = nn.ModuleList([Block(chs[i], chs[i+1]) for i in range(len(chs)-1)])
+        self.pool       = nn.MaxPool2d(2)
+    
+    def forward(self, x):
+        ftrs = []
+        for block in self.enc_blocks:
+            x = block(x)
+            ftrs.append(x)
+            x = self.pool(x)
+        return ftrs
+
+
+class Decoder(nn.Module):
+    def __init__(self, chs=(256, 128, 64, 32, 16)):
+        super().__init__()
+        self.chs         = chs
+        self.upconvs    = nn.ModuleList([nn.ConvTranspose2d(chs[i], chs[i+1], 2, 2) for i in range(len(chs)-1)])
+        self.dec_blocks = nn.ModuleList([Block(chs[i], chs[i+1]) for i in range(len(chs)-1)]) 
+        
+    def forward(self, x, encoder_features):
+        for i in range(len(self.chs)-1):
+            x        = self.upconvs[i](x)
+            enc_ftrs = self.crop(encoder_features[i], x)
+            x        = torch.cat([x, enc_ftrs], dim=1)
+            x        = self.dec_blocks[i](x)
+        return x
+    
+    def crop(self, enc_ftrs, x):
+        _, _, H, W = x.shape
+        enc_ftrs   = torchvision.transforms.CenterCrop([H, W])(enc_ftrs)
+        return enc_ftrs
+
+
+class Unet(nn.Module):
+    def __init__(self, enc_chs=(2,16,32,64,128,256), dec_chs=(256, 128, 64, 32, 16), num_fields=3, retain_dim=False, out_sz=(256,256)):
+        super().__init__()
+        self.encoder     = Encoder(enc_chs)
+        self.decoder     = Decoder(dec_chs)
+        self.head        = nn.Conv2d(dec_chs[-1], num_fields, 1)
+        # self.relu        = nn.ReLU()
+        self.sigmoid     = nn.Sigmoid()
+        self.retain_dim  = retain_dim
+
+    def forward(self, x):
+        enc_ftrs = self.encoder(x)
+        out      = self.decoder(enc_ftrs[::-1][0], enc_ftrs[::-1][1:])
+        # out      = self.relu(self.head(out))
+        out      = self.sigmoid(self.head(out))
+        if self.retain_dim:
+            out = F.interpolate(out, out_sz)
+        return out
+
+
+
+
